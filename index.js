824857de8447b9fd2a2cc0022fe4b683719ed812
Orlando <prorlandotroncoso@gmail.com>
2026-05-24 21:19:10 -0300

    remove unicode separators

--- index.js
+++ index.js
@@ -288,6 +288,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 
-  console.log(\`Servidor funcionando en puerto ${PORT}`);
+  console.log(`Servidor funcionando en puerto ${PORT}`);
 
 });
