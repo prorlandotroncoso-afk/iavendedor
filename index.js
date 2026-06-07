1e6c35e058b1b2ec651ea746ab48d479d4f07926
Orlando <prorlandotroncoso@gmail.com>
2026-06-07 17:43:28 -0300

    Sistema PDFs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use("/pdfs", express.static("pdfs"));
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 });
